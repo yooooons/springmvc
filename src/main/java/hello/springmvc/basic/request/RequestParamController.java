fb419c9fecc4bb08e70f9bb2e1d1194ebd728181
YS <yooooonnf@gmail.com>
2022-02-11 23:25:46 +0900

    스프링 MVC - 기본 기능

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,12 +1,17 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,4 +24,95 @@ public class RequestParamController {
         response.getWriter().write("OK");
 
     }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username")String memberName,
+                                 @RequestParam("age")int memberAge) {
+        log.info("username={}, age={}", memberName, memberAge);
+        return "OK";
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) {
+        log.info("username={}, age={}", username, age);
+        return "OK";
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username,  age);
+        return "OK";
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+        //int는 null이 될수 없다.따라서 Integer은 null값을 넣을수 있다(객체라서)
+        //null,"" 구분
+        log.info("username={}, age={}", username, age);
+        return "OK";
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true,defaultValue = "guest") String username,
+            @RequestParam(required = false,defaultValue = "1") int age) {
+        //default가 들어가면 굳이 required는 필요가 없다 있든없든 대체값이 들어가기 때문에
+        log.info("username={}, age={}", username, age);
+        return "OK";
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMAp(@RequestParam Map<String,Object> paramMap) {
+        //default가 들어가면 굳이 required는 필요가 없다 있든없든 대체값이 들어가기 때문에
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "OK";
+
+    }
+
+
+    /*
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@RequestParam String username,
+                                   @RequestParam int age) {
+        HelloData helloData = new HelloData();
+        helloData.setUsername(username);
+        helloData.setAge(age);
+
+        log.info("username={}, age={}", helloData.getUsername(),helloData.getAge());
+        return "OK";
+    }
+    이것의 발전형태
+    */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),helloData.getAge());
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2( HelloData helloData) {
+                log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "OK";
+    }
+
+
+
 }
